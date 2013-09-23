73a4d5a7a6e2ec8599f2ce24a15add2a6763f596
Philipp Muens <raute1337@gmx.de>
2013-09-23 18:45:33 +0200

    - Removed the datasets-Controller (with all it's dependencies) - Added the actions for each example in the examples-Controller - Added a title-helper in the application.haml-File (this one is for displaying a correct html title) - Added a title in the actions for each example - Added routings to the correct controller-actions (for each example) - Added a view for each example

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,5 +1,29 @@
 class ExamplesController < ApplicationController
   def index
-    
+    @title = 'Overview of examples'
+  end
+
+  def line
+    @title = 'Line Demo'
+  end
+
+  def bar
+    @title = 'Bar Demo'
+  end
+
+  def radar
+    @title = 'Radar Demo'
+  end
+
+  def polar_area
+    @title = 'Polar Area Demo'
+  end
+
+  def pie
+   @title = 'Pie Demo' 
+  end
+
+  def doughnut
+    @title = 'Doughnut Demo'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 ExampleSqliteChartjs::Application.routes.draw do
   root to: 'examples#index'
+
+  get '/line', to: 'examples#line'
+  get '/bar', to: 'examples#bar'
+  get '/radar', to: 'examples#radar'
+  get '/polar_area', to: 'examples#polar_area'
+  get '/pie', to: 'examples#pie'
+  get '/doughnut', to: 'examples#doughnut'
 end
