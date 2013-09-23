3266ea58ba128fcf359314c2f35ff5dfe5300f33
Philipp Muens <raute1337@gmx.de>
2013-09-23 17:06:30 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExampleSqliteChartjs::Application.config.secret_key_base = '4a53baa29362389434687c011c3a73f3ab61431bedea3ef70602500eeb7bdd01dfdfe3303b30de12cc4d88cfe58dbbf13d69a7c72a1c00ce41e260b8c29528b8'
