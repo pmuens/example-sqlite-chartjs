21371563e78209b93863e58accca96e8fcefbf20
Philipp Muens <raute1337@gmx.de>
2013-09-24 12:02:26 +0200

    - Added respond_to json, xml and html blocks in the examples_controller - Added a well which describes the available APIs in the examples-Index-View

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -4,30 +4,65 @@ class ExamplesController < ApplicationController
   def index
     @title = 'Overview of examples'
     @datasets = Dataset.all
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def line
     @title = 'Line Demo'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def bar
     @title = 'Bar Demo'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def radar
     @title = 'Radar Demo'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def polar_area
     @title = 'Polar Area Demo'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def pie
-   @title = 'Pie Demo' 
+   @title = 'Pie Demo'
+   respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   def doughnut
     @title = 'Doughnut Demo'
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @datasets.to_xml }
+      format.json { render json: @datasets }
+    end
   end
 
   private
